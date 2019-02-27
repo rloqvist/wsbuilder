3ab8e69edf31807d4c055e10511b1310a2761b25
rloqvist <rasmus@catfarm.se>
2019-02-27 12:44:06 +0100

    Added select typw

--- src/components/ExerciseContainer.js
+++ src/components/ExerciseContainer.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
-import { Card, CardBody, CardTitle, CardSubtitle, CardText, Button } from 'reactstrap';
+import { Card, CardBody, CardTitle, CardSubtitle, CardText, Button, Input, Form, FormGroup } from 'reactstrap';
+import {SelectTypeComponent} from './exercises/SelectTypeComponent';
 
 const StyledExerciseWrapper = styled.div`
   padding: 10px 0;
@@ -23,14 +24,23 @@ const DisplayExercise = args => {
 const EditExercise = args => {
   console.log("EditExercise args:", args);
   return (
-    <Card>
-      <CardBody>
-        <CardTitle>{args.title}<Button color="link" onClick={() => args.onDelete(args.id)}>delete</Button></CardTitle>
-        <CardSubtitle>{args.exerciseType}</CardSubtitle>
-        <CardText>{args.description}</CardText>
+    <Form>
+      <FormGroup>
+        <Input placeholder="Name your exercise" defaultValue={args.title} />
+      </FormGroup>
+
+      <FormGroup>
+        <SelectTypeComponent />
+      </FormGroup>
+
+      <FormGroup>
+        <Input type="textarea" defaultValue={args.description} />
+      </FormGroup>
+
+      <FormGroup>
         <Button onClick={args.toggleEdit}>Finished editing</Button>
-      </CardBody>
-    </Card>
+      </FormGroup>
+    </Form>
   )
 }
 
--- src/components/exercises/SelectTypeComponent.js
+++ src/components/exercises/SelectTypeComponent.js
@@ -1,1 +1,43 @@
 import React, {Component} from 'react';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+
+const options = [
+  'PlainExercise',
+  'MultisetExercise',
+  'InlineExercise',
+  'CardioExercise',
+];
+
+export class SelectTypeComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: false,
+      type: this.props.exerciseType || 'Select exercise type',
+    };
+  }
+
+  toggle = (args) => {
+    console.log('args', args);
+    this.setState({open: !this.state.open})
+  }
+
+  handleSetType = type => {
+    console.log('type', type);
+    this.setState({type})
+  }
+
+  render() {
+    return (
+      <Dropdown isOpen={this.state.open} toggle={this.toggle}>
+        <DropdownToggle caret>{this.state.type}</DropdownToggle>
+        <DropdownMenu>
+          {options.map((option, index) =>{
+            return <DropdownItem key={option} onClick={() => this.handleSetType(option)}>{option}</DropdownItem>
+          })}
+        </DropdownMenu>
+      </Dropdown>
+    )
+  }
+}
